952a88f2c4a89d15ef5baa667690f0a46754a4be
genaro-ambia <A01420567@tec.mx>
2022-10-29 14:44:51 -0500

    Lab 16 sesiones

--- Lab_16/app.js
+++ Lab_16/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -15,6 +16,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cookieParser());
 
++app.use(session({
+    secret: 'shfjdfhefwbfns475fe4db37bdnewnudnw94373bnda', 
+    resave: false,  
+    saveUninitialized: false, 
+}));
+
 app.use((request, response, next) => {
     const clicks = Number(request.cookies.numero_clicks ? request.cookies.numero_clicks : 0) + 1;
     console.log(request.cookies);
@@ -23,8 +30,9 @@ app.use((request, response, next) => {
 });
 
 const rutasMaterias = require('./routes/materias.routes');
-const cookieParser = require('cookie-parser');
 app.use('/materias', rutasMaterias);
+const rutasUsuarios = require('./routes/user.routes');
+app.use('/user', rutasUsuarios);
 
 app.use((request, response, next) => {
     response.status(404);
