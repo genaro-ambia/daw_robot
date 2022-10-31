55019afcdb2523178da768d39575857cf33330cf
genaro-ambia <A01420567@tec.mx>
2022-10-30 20:01:05 -0600

    rework

--- Lab_17a/controllers/user.controller.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-exports.logout = (request, response, next) => {
-    request.session.destroy(() => {
-        response.redirect('/robots/'); //Este código se ejecuta cuando la sesión se elimina.
-    });
-};
