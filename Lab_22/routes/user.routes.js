e469c8f90866b3ae3571bd94589b548a24b3f7d5
genaro-ambia <A01420567@tec.mx>
2022-11-16 11:10:19 -0600

    Lab 22 evidencia

--- Lab_22/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userController = require('../controllers/user.controller');
-
-const router = express.Router();
-
-router.get('/logout', userController.logout);
-
-module.exports = router;
